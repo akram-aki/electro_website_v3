ca08d411bd1fdce847a3ae640217075db32ed95e
youcefboubidi <youcefboubidi3@gmail.com>
2025-03-18 23:52:45 +0100

    reduced welcome day image size

--- src/components/stealScrollEvents/Items.jsx
+++ src/components/stealScrollEvents/Items.jsx
@@ -108,7 +108,7 @@ export default function Items({ count, ...attributes }) {
                   count === 0
                     ? "/IMG_0110.JPG"
                     : count === 1
-                      ? "/welcomeDay.JPG"
+                      ? "/welcomeDay-min.JPG"
                       : count === 2
                         ? "/IMG_20240516_042629_313.jpg"
                         : "/TEKKID.png"
